eb501abf9f57c3610618dd82b0527f843f74df00
Dione Wilson <dionew1@gmail.com>
2018-09-11 15:12:04 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+ Store.create!(
+    title: Faker::Company.name,
+    street: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip
+ )
+end
+
+
+30.times do
+ Pet.create!(
+  name: Faker::Dog.name,
+  breed: Faker::Dog.breed,
+  age: Faker::Dog.age,
+  gender: Faker::Dog.gender,
+  description: Faker::Lorem.paragraph,
+  store: Store.find(rand(1..10))
+ )
+end
